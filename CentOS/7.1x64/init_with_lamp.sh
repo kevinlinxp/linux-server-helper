740082afcb454301b23bbfd953fb2f2d1d550828
Ke Lin <cqqlin@gmail.com>
2016-02-19 00:14:16 +1030

    Update init_with_lamp.sh

--- CentOS/7.1x64/init_with_lamp.sh
+++ CentOS/7.1x64/init_with_lamp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+VERSION_PHPMYADMIN=4.5.4.1
+TIME_ZONE=Australia/Adelaide
+
+
 sudo mv /etc/localtime /etc/localtime.bak
-sudo ln -s /usr/share/zoneinfo/Australia/Adelaide /etc/localtime
+sudo ln -s /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime
 
 sudo yum -y update
 sudo yum -y install epel-release
@@ -15,8 +20,9 @@ sudo yum -y install mariadb-server mariadb
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum -y install php55w php55w-cli php55w-common php55w-gd php55w-mcrypt php55w-mysqlnd php55w-xml php55w-soap
+
 # sudo yum -y install phpmyadmin
-curl -S https://files.phpmyadmin.net/phpMyAdmin/4.5.4.1/phpMyAdmin-4.5.4.1-english.tar.gz | tar zxv -C /var/www/html
+curl -S https://files.phpmyadmin.net/phpMyAdmin/${VERSION_PHPMYADMIN}/phpMyAdmin-${VERSION_PHPMYADMIN}-english.tar.gz | tar zxv -C /var/www/html
 
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
