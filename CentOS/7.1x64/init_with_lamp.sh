6e1d8311bda1d0e3462e689a88fd9889ae9cd1ff
cqqlin <cqqlin@gmail.com>
2016-02-19 00:09:36 +1030

    centos 7.1x64 init with lamp

---
+++ CentOS/7.1x64/init_with_lamp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo mv /etc/localtime /etc/localtime.bak
+sudo ln -s /usr/share/zoneinfo/Australia/Adelaide /etc/localtime
+
+sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y clean all
+
+sudo yum -y install git
+sudo yum -y install zip unzip
+sudo yum -y tree
+sudo yum -y install httpd
+sudo yum -y install mariadb-server mariadb
+
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+sudo yum -y install php55w php55w-cli php55w-common php55w-gd php55w-mcrypt php55w-mysqlnd php55w-xml php55w-soap
+# sudo yum -y install phpmyadmin
+curl -S https://files.phpmyadmin.net/phpMyAdmin/4.5.4.1/phpMyAdmin-4.5.4.1-english.tar.gz | tar zxv -C /var/www/html
+
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+sudo mysql_secure_installation
