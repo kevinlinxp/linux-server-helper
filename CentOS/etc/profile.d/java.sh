87bc81881bde9eb2f6919f22fc64a632166d962c
Ke Lin <cqqlin@gmail.com>
2016-02-19 22:17:59 +1030

    Update java.sh

--- CentOS/etc/profile.d/java.sh
+++ CentOS/etc/profile.d/java.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-JAVA_HOME=/usr/java/jdk1.8.0_25/
+JAVA_HOME=/usr/java/jdk1.8.0_25
+M2_HOME=/opt/maven/apache-maven
 PATH=$JAVA_HOME/bin:$PATH
-export PATH JAVA_HOME
+export PATH M2_HOME JAVA_HOME
 #export CLASSPATH=.
