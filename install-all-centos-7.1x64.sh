a5d1ad6165a5c7492ba413a919f4a80784b3ac4f
Kevin Lin <kevinlinxp@gmail.com>
2017-09-03 23:20:12 +0930

    revise

--- install-all-centos-7.1x64.sh
+++ install-all-centos-7.1x64.sh
@@ -67,4 +67,3 @@ sudo chkconfig mongod on
 ## Install nodejs
 sudo yum install epel-release
 sudo rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm && yum -y install nodejs
-sudo npm install npm@latest -g
--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo yum install epel-release
 sudo rpm -ivh https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm && yum -y install nodejs
-sudo npm install npm@latest -g
